4f3f27c602d718b45c8dfb8d89202fb5ed814aa7
Namhyuck (James) Kim <namhyuck.james@gmail.com>
2025-07-30 00:54:02 +0900

    Fix home-manager version

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -63,13 +63,9 @@ in
             sharedFiles
             additionalFiles
           ];
-          stateVersion = "23.11";
+          stateVersion = "24.05";
         };
         programs = { } // import ../shared/home-manager.nix { inherit config pkgs lib; };
-
-        # Marked broken Oct 20, 2022 check later to remove this
-        # https://github.com/nix-community/home-manager/issues/3344
-        manual.manpages.enable = false;
       };
   };
 
