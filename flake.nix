9f0ad979cbe65df9c919ff3f51c30c7598a863a5
Namhyuck (James) Kim <namhyuck.james@gmail.com>
2025-07-30 01:10:19 +0900

    revert zellij

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-homebrew = {
-      url = "github:zhaofengli-wip/nix-homebrew/master";
+      url = "github:zhaofengli-wip/nix-homebrew";
     };
     homebrew-bundle = {
       url = "github:homebrew/homebrew-bundle";
