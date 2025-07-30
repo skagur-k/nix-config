01c039cdaedc2b99f98cab16023bb7a6b84dbfe0
Namhyuck (James) Kim <namhyuck.james@gmail.com>
2025-07-30 21:40:04 +0900

    update preview

--- modules/shared/config/zsh/env.zsh
+++ modules/shared/config/zsh/env.zsh
@@ -2,7 +2,7 @@ export FZF_DEFAULT_COMMAND="fd --type f --hidden --exclude .git"
 export FZF_DEFAULT_OPTS="--height 40% --border"
 
 export SKIM_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
-export SKIM_DEFAULT_OPTIONS='--ansi --height 40% --border --preview ([[ -d {} ]] && eza -lah --color=always {} || bat --style=numbers --color=always --line-range :500 {})'
+export SKIM_DEFAULT_OPTIONS='--ansi --height 40% --border --preview "if [ -d {} ]; then eza -lah --color=always {}; else bat --style=numbers --color=always --line-range :500 {}; fi"'
 
 # Set editor
 export EDITOR=helix
