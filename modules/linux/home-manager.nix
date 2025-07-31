2e49c1483bc68ed17fef2310b82328e336bc7dc3
Namhyuck (James) Kim <namhyuck.james@gmail.com>
2025-07-31 23:50:06 +0900

    Add required home.stateVersion and complete Linux Home Manager configuration

--- modules/linux/home-manager.nix
+++ modules/linux/home-manager.nix
@@ -5,7 +5,26 @@
   ...
 }:
 
+let
+  user = "skagur";
+  sharedFiles = import ../shared/files.nix { inherit config pkgs; };
+  additionalFiles = import ./files.nix { inherit user config pkgs; };
+in
 {
+  home = {
+    username = user;
+    homeDirectory = "/home/${user}";
+    stateVersion = "24.05";
+    enableNixpkgsReleaseCheck = false;
+    packages = pkgs.callPackage ./packages.nix { };
+    file = lib.mkMerge [
+      sharedFiles
+      additionalFiles
+    ];
+  };
+
+  programs = { } // import ../shared/home-manager.nix { inherit config pkgs lib; };
+
   # WSL2 specific environment variables
   home.sessionVariables = {
     WSLENV = "1";
