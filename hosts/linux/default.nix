0f1f615db59a3968e0fb47537f8ebdf76194c378
Namhyuck (James) Kim <namhyuck.james@gmail.com>
2025-07-31 23:24:33 +0900

    Fix host

--- hosts/linux/default.nix
+++ hosts/linux/default.nix
@@ -13,22 +13,24 @@ in
     useGlobalPkgs = true;
     backupFileExtension = "backup";
     verbose = false;
-    users.${user} = {
-      pkgs,
-      config,
-      lib,
-      ...
-    }: {
-      home = {
-        enableNixpkgsReleaseCheck = false;
-        packages = pkgs.callPackage ../../modules/linux/packages.nix { };
-        file = lib.mkMerge [
-          (import ../../modules/shared/files.nix { inherit config pkgs; })
-          (import ../../modules/linux/files.nix { inherit config pkgs; })
-        ];
-        stateVersion = "24.05";
+    users.${user} =
+      {
+        pkgs,
+        config,
+        lib,
+        ...
+      }:
+      {
+        home = {
+          enableNixpkgsReleaseCheck = false;
+          packages = pkgs.callPackage ../../modules/linux/packages.nix { };
+          file = lib.mkMerge [
+            (import ../../modules/shared/files.nix { inherit config pkgs; })
+            (import ../../modules/linux/files.nix { inherit config pkgs; })
+          ];
+          stateVersion = "24.05";
+        };
+        programs = { } // import ../../modules/shared/home-manager.nix { inherit config pkgs lib; };
       };
-      programs = { } // import ../../modules/shared/home-manager.nix { inherit config pkgs lib; };
-    };
   };
-} 
+}
